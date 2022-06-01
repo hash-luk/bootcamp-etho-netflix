afcdd966cd63304caafb7f878a582219a1d619f0
hash-luk <lucasgamerhd18@gmail.com>
2022-05-31 22:02:23 -0300

    Added inputs and yup validation

--- src/screens/login/login.styled.tsx
+++ src/screens/login/login.styled.tsx
@@ -2,6 +2,30 @@ import styled from "styled-components";
 import { Grid } from "@mui/material";
 
 export const Wrapper = styled(Grid)`
-    min-height: 100vh;
-    background-color: #1A1A1A;
+  min-height: 100vh;
+  background-color: #1a1a1a;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 8px;
+  margin: 0 0 22px;
+  border-radius: 5px;
+  border: 1px solid #e0e0e0;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  padding: 9px;
+  margin: 0 0 22px;
+  border-radius: 5px;
+  background-color: #e31a13;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+`;
+
+export const Error = styled.p`
+ color: #e31a13;
+ font-weight: 800;
 `;
--- src/screens/login/login.tsx
+++ src/screens/login/login.tsx
@@ -1,17 +1,59 @@
-import { useCallback } from 'react';
-import { Wrapper } from './login.styled';
+import { Grid } from "@mui/material";
+import * as yup from "yup";
+import React, { useCallback, useEffect, useState } from "react";
+import * as Styled from "./login.styled";
 
 export default function Login() {
-    const handleChange = useCallback(
-        (event: any) => {
-            console.log(event.target.value);
-        },
-        []
-    )
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
 
-    return(
-        <Wrapper container justifyContent="center" alignContent="center">
-            <input type="email" name="email" onChange={handleChange}/>
-        </Wrapper>
-    )
+  const [error, setError] = useState("");
+
+  const handleChange = useCallback(
+    ({ target }: any) => {
+      setData((prevData) => ({
+        ...prevData,
+        [target.name]: target.value,
+      }));
+    },
+    [setData]
+  );
+
+  const handleSubmit = useCallback(async () => {
+    try {
+      const schema = yup.object().shape({
+        email: yup.string().email().required(),
+        password: yup.string().required(),
+      });
+
+      await schema.validate(data);
+      setError("")
+    } catch (error: any) {
+      setError(error.errors[0]);
+      console.log(error);
+    }
+  }, [data]);
+
+  return (
+    <Styled.Wrapper container justifyContent="center" alignContent="center">
+      <Grid xs={2} container justifyContent="center" alignContent="center">
+        <Styled.Input
+          type="email"
+          name="email"
+          placeholder="E-mail"
+          onChange={handleChange}
+        />
+        <Styled.Input
+          type="text"
+          name="password"
+          placeholder="Senha"
+          onChange={handleChange}
+        />
+        <Styled.Button onClick={handleSubmit}>Entrar</Styled.Button>
+        <Styled.Error>{error}</Styled.Error>
+      </Grid>
+    </Styled.Wrapper>
+  );
 }
